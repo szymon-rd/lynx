55da5b778fb9b075ef5a6d245a3b2e2fc3a02991
Szymon Rodziewicz <szymonrodant@gmail.com>
2024-01-05 21:02:12 +0100

    Example of abstract service in tests

--- core/src/test/scala/com/virtuslab/lynx/data/StateTest.scala
+++ core/src/test/scala/com/virtuslab/lynx/data/StateTest.scala
@@ -1,5 +1,7 @@
 package com.virtuslab.lynx.data
 
+import com.virtuslab.lynx.*
+
 class StateTest extends munit.FunSuite {
 
   test("nextInt") {
@@ -19,5 +21,39 @@ class StateTest extends munit.FunSuite {
     assertEquals(i1, 0)
     assertEquals(i2, 1)
     assertEquals(i3, 2)
+  }
+
+
+
+  test("userService") {
+    case class User(name: String)
+    trait UserService[Cap[_]] {
+      def addUser(name: String): Unit in Cap
+      def getUser(name: String): Option[User] in Cap
+    }
+
+    type Storage[A] = StateM[List[User], A]
+    case class StateUserService() extends UserService[Storage] {
+
+      override def addUser(name: String): Unit in Storage = {
+        println("Adding user: " + name)
+        val newUser = User(name)
+        State[List[User]].update(newUser :: _)
+      }
+        
+      override def getUser(name: String): Option[User] in Storage =
+        println("Getting user: " + name)
+        State[List[User]].get().find(_.name == name)
+    }
+
+    def program[F[_]](userService: UserService[F]): Option[User] in F = {
+      userService.addUser("John")
+      userService.addUser("Jane")
+      userService.getUser("John")
+    }
+
+    val (state, result) = State[List[User]].reify(program(StateUserService()))(List.empty)
+    assert(state == List(User("Jane"), User("John")))
+    assert(result == Some(User("John")))
   }
 }
